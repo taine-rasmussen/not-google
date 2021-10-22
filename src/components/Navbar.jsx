443ce8628589371c5e8b8eefa1062e47647da753
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-22 22:28:14 +1300

    added Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Navbar = () => {
    return (
-      <div>
+      <div className='p-5 pb-0 flex flex-wrap sm:justify-between justift-center items-center border-b dark:border-gray-700 border-gray-200'>
          <h1>Navbar</h1>
       </div>
    )
