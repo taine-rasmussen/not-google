617ca57296b5b33d99f8dd7554e4aa793ed1ae2f
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-23 14:57:56 +1300

    set up react-router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
    return (
       <div className='p-5 pb-0 flex flex-wrap sm:justify-between justift-center items-center border-b dark:border-gray-700 border-gray-200'>
          <div className='flex justify-between items-center space-x-5 w-screen'>
-
+            <Link>
+               <p className='text-2xl'>
+                  Oogle 🔍
+               </p>
+            </Link>
          </div>
       </div>
    )
