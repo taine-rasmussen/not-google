14a8b8a93e954a17e3c1bedca97196a8894542e5
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-23 14:53:58 +1300

    added npm pacakges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const Navbar = () => {
    return (
       <div className='p-5 pb-0 flex flex-wrap sm:justify-between justift-center items-center border-b dark:border-gray-700 border-gray-200'>
-         <h1>Navbar</h1>
+         <div className='flex justify-between items-center space-x-5 w-screen'>
+
+         </div>
       </div>
    )
 }
