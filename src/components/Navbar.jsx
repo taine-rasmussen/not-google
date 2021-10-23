69be8c419fb676e1aa148aee2b822f96c5b614e2
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-23 14:59:58 +1300

    added styling to navbar header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
       <div className='p-5 pb-0 flex flex-wrap sm:justify-between justift-center items-center border-b dark:border-gray-700 border-gray-200'>
          <div className='flex justify-between items-center space-x-5 w-screen'>
             <Link>
-               <p className='text-2xl'>
+               <p className='text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900'>
                   Oogle 🔍
                </p>
             </Link>
