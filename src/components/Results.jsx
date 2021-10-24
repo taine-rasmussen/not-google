15c3494965e40893a903568de87d4198a640cf62
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-24 21:47:02 +1300

    added jsx for search results return

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -13,7 +13,7 @@ export const Results = () => {
    const location = useLocation();
 
    useEffect(() => {
-       getResults('/search/q=Wellington&num=40')
+       getResults('/search/q=Calvin and Hobbes&num=40')
    }, []);
 
    if (isLoading) return <Loading />;
@@ -22,7 +22,18 @@ export const Results = () => {
 
    switch(location.pathname){
       case '/search':
-         return 'Search';
+         return (
+                  <div className="sm:px-56 flex flex-wrap justify-between space-y-6">
+                     {results?.results?.map(({ link, title }, index) => (
+                        <div key={index} className="md:w-2/5 w-full">
+                        <a href={link} target="_blank" rel="noreferrer">
+                           <p className="text-sm">{link.length > 30 ? link.substring(0, 30) : link}</p>
+                           <p className="text-lg hover:underline dark:text-blue-300 text-blue-700  ">{title}</p>
+                        </a>
+                        </div>
+                     ))}
+                  </div>
+                );
       case '/news':
          return 'News';
       case '/images':
