429c8bbaf0ef83ddad5a7d0c93d28d669fc8baeb
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-24 21:07:03 +1300

    Created loading component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+
+import { useResultContext } from '../Contexts/ResultsContextProvider';
+import { Loading } from './Loading'
+
+
 
 export const Results = () => {
+
+   const {getResults, results, searchTerm, setSearchTerm, isLoading} = useResultContext();
+   const location = useLocation();
+
+   if (isLoading) return <Loading />;
+
+
    return (
       <div>
          Results
