1f227493d35c0a4c829fe9cd8c5bda5c776892bd
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-10-24 22:07:09 +1300

    images display

--- src/Contexts/ResultsContextProvider.js
+++ src/Contexts/ResultsContextProvider.js
@@ -6,7 +6,7 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('Calvin and Hobbes');
 
   // GET api data, converts to json and updates results hook with new data
   const getResults = async (url) => {
